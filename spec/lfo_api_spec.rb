3e7fef2fc7dcf672c32699381f034045d604bd19
nealdeters <nealdeters@gmail.com>
2016-04-07 15:35:00 -0500

    added more tests for spec

--- spec/lfo_api_spec.rb
+++ spec/lfo_api_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
+# The hypothetical API is accessible via a standard HTTP GET request. For example:
+
+# http://not_real.com/customer_scoring?income=50000&zipcode=60201&age=35
+
+# A successful request to the API will return a JSON response resembling the following:
+
+# {
+#   "propensity": 0.26532,
+#   "ranking": "C"
+# }
+
 describe LfoApi::Client do
   let(:url){LfoApi::Client.new}
 
@@ -17,6 +28,20 @@ describe LfoApi::Client do
     result = url.results.class
 
     expect(result).to eq(Array)
+  end
+
+  it 'should search by propensity' do
+    result = url.income("0.26532")
+    result = result[0]["propensity"]
+    
+    expect(result).to eq("0.26532")
+  end
+
+  it 'should search by ranking' do
+    result = url.income("C")
+    result = result[0]["ranking"]
+    
+    expect(result).to eq("C")
   end
 
   it 'should search by income' do
